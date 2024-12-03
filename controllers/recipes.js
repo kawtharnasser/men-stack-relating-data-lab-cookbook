647e04ef7066beab9ce42a31fd8055b75c04c865
kawtharnasser <kawtherhabib.ko@gmail.com>
2024-12-03 21:42:16 +0300

    completed recipe

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -10,10 +10,16 @@ const Recipe = require('../models/recipe.js');
 
 //Root Rout
 router.get('/', async (req, res) => {
-  res.render('recipes/index.ejs');
+  try{
+    const recipes = await Recipe.find().populate('owner')
+    res.render('recipes/index.ejs',{recipes});
+  }catch(error){
+    console.log(error)
+    redirect('/')
+  }
 });
 
-//add New
+//add New functionality
 router.get('/new', async(req,res)=>{
   res.render('recipes/new.ejs')
 })
@@ -28,6 +34,50 @@ router.post('/', async (req,res)=>{
     res.redirect('/')
   }
 })
+
+// show functionality "display = get"
+router.get('/:recipeId', async(req,res)=>{
+  const recipe = await Recipe.findById(req.params.recipeId).populate('owner')
+  res.render('recipes/show.ejs',{recipe})
+})
+
+//delete functionality
+router.delete('/:recipeId', async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.recipeId);
+    if (recipe.owner.equals(req.session.user._id)) {
+      await recipe.deleteOne();
+      res.redirect('/recipes');
+    } else {
+      res.send("You don't have permission to do that.");
+    }
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
+});
+
+//edit functionality
+router.get('/:recipeId/edit', async(req,res)=>{
+  const recipe = await Recipe.findById(req.params.recipeId)
+  res.render('recipes/edit.ejs',{recipe})
+})
+
+router.put('/:recipeId', async(req,res)=>{
+  try{
+    const recipe = await Recipe.findById(req.params.recipeId)
+    if(recipe.owner.equals(req.session.user._id)){
+      await recipe.updateOne(req.body)
+      res.redirect('/recipes')
+    }else{
+      res.send("You don't have permission to do that.")
+    }
+  }catch(error){
+    console.log(error)
+    res.redirect('/')
+  }
+})
+
 
 
 
